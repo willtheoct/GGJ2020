de39ffa0a3b77c656261f82be587a41a8e07bfa2
Quang <quwanger@gmail.com>
2020-02-01 14:19:32 -0500

    added hint screens and buttons, started on button code

---
+++ Assets/HintButton1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintButton1 : MonoBehaviour
+{
+    public GameObject StartOptions;
+    public GameObject HintScreen1;
+
+    void OnMouseDown()
+    {
+        StartOptions.SetActive(false);
+        HintScreen1.SetActive(true);
+
+    }
+}
+
---
+++ Assets/HintButton2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintButton2 : MonoBehaviour
+{
+    public GameObject StartOptions;
+    public GameObject HintScreen2;
+
+    void OnMouseDown()
+    {
+        StartOptions.SetActive(false);
+        HintScreen2.SetActive(true);
+
+    }
+}
