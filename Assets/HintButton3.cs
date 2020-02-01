76168fdc194e3a0f9b1ba164bd89c1e3caca38a9
Quang <quwanger@gmail.com>
2020-02-01 14:27:55 -0500

    added hit button code and potential music docx

---
+++ Assets/HintButton3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintButton3 : MonoBehaviour
+{
+    public GameObject StartOptions;
+    public GameObject HintScreen3;
+
+    void OnMouseDown()
+    {
+        StartOptions.SetActive(false);
+        HintScreen3.SetActive(true);
+
+    }
+}
---
+++ Assets/HintButton4.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintButton4 : MonoBehaviour
+{
+    public GameObject StartOptions;
+    public GameObject HintScreen4;
+
+    void OnMouseDown()
+    {
+        StartOptions.SetActive(false);
+        HintScreen4.SetActive(true);
+
+    }
+}
