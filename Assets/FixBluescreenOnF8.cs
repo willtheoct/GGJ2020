16f45e27285a9759e003d38314778518eee8b2a8
William Weissman <willthingy@hotmail.com>
2020-01-31 19:50:38 -0500

    bluescreen disappears on f8

---
+++ Assets/FixBluescreenOnF8.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixBluescreenOnF8 : MonoBehaviour
+{
+    public GameObject BlueScreen;
+    void Update()
+    {
+        if (Input.GetButtonDown("f8"))
+        {
+            BlueScreen.SetActive(false);
+        }
+    }
+}
