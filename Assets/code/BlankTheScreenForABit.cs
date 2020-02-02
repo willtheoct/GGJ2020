c69b917ec719efa4b6bab72ec4c23afd11740b3f
Quang <quwanger@gmail.com>
2020-02-02 09:14:13 -0500

    reorganized assets, scene populated more, black screen before blue screen

---
+++ Assets/code/BlankTheScreenForABit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlankTheScreenForABit : MonoBehaviour
+{
+    public float timer = 0.4f;
+    void Start()
+    {
+        Camera.main.cullingMask = 0;
+        Camera.main.clearFlags = CameraClearFlags.SolidColor;
+        Camera.main.backgroundColor = Color.black;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Camera.main.cullingMask = 0xffff;
+        }
+    }
+}
---
+++ Assets/code/PlayButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject TitleScreen;
+    public GameObject BlueScreen;
+    public GameObject blankTheScreen;
+    
+    void OnMouseDown ()
+    {
+        TitleScreen.SetActive(false);
+        BlueScreen.SetActive(true);
+        blankTheScreen.SetActive(true);
+    }
+}
