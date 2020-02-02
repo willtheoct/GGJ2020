2b86459c84682c58eb770af394cb7472e6d3651d
Quang <quwanger@gmail.com>
2020-02-01 21:32:53 -0500

    the computer now randomly has redraw errors

---
+++ Assets/RandomlyBugTheScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomlyBugTheScreen : MonoBehaviour
+{
+    public computer pc;
+    float timer = 20f;
+    void Start()
+    {
+        transform.parent = null;
+        timer = 20f;
+    }
+    void Update()
+    {
+        if (!pc.bug.activeSelf)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer < 0f)
+        {
+            pc.bug.SetActive(true);
+            timer = 20f + Random.Range(0f, 20f);
+        }
+    }
+}
