1231e77d41145a6e532db53df88d08c2eddef52d
Quang <quwanger@gmail.com>
2020-02-02 14:34:53 -0500

    code to go back to titlescreen

---
+++ Assets/code/Esctoplaymenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    public GameObject VicotryScreen;
+    public GameObject TitleScreen;
+    void OnMouseDown()
+    {
+        VicotryScreen.SetActive(false);
+        TitleScreen.SetActive(true);
+    }
+}
