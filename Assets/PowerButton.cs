100db4e7217b48f3a33910bff5ab9d5b5d286902
Quang <quwanger@gmail.com>
2020-02-01 21:27:10 -0500

    added power button starting color and changing color

--- Assets/PowerButton.cs
+++ Assets/PowerButton.cs
@@ -8,6 +8,18 @@ public class PowerButton : MonoBehaviour
     void OnMouseDown()
     {
         screen.SetActive(!screen.activeSelf);
+        if (screen.activeSelf)
+        {
+            GetComponent<MeshRenderer>().material.color = new Color(0f, 1f, 0f, 1f);
+
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material.color = new Color(1f, 0f, 0f, 1f);
+        }
     }
-    
+    void Start()
+    {
+        GetComponent<MeshRenderer>().material.color = new Color(1f, 0f, 0f, 1f);
+    }
 }
