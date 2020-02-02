5f14f48808f9294e98de536e060a6d4440caab1c
Quang <quwanger@gmail.com>
2020-02-02 13:16:01 -0500

    Started code to end the game

--- Assets/code/ItemsInTrash.cs
+++ Assets/code/ItemsInTrash.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class ItemsInTrash : MonoBehaviour
 {
+    public GameObject Bug;
+    public GameObject MainCamera;
+    public GameObject Screen;
+    public float maxdistance = 1F;
+    public float ZoomSpeed = 0.02F;
     void OnTriggerEnter(Collider C)
     {
         if (C.GetComponent<MakeNoiseOnSmack>() != null)
         {
             C.transform.parent = transform;
-            if (transform.childCount > 10)
+            if (transform.childCount > 3 && Bug == null && )
             {
-
+                if ((MainCamera.transform.position - Screen.transform.position).magnitude < maxdistance)
+                {
+                    MainCamera.transform.Translate(Vector3.forward * Time.deltaTime * ZoomSpeed);
+                }
             }
         }
     }
