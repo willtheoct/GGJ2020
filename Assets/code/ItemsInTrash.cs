224f64e5025c89b8a4561870ab017f2be994b3df
Quang <quwanger@gmail.com>
2020-02-02 14:13:03 -0500

    most victory stuff hooked up

--- Assets/code/ItemsInTrash.cs
+++ Assets/code/ItemsInTrash.cs
@@ -8,19 +8,33 @@ public class ItemsInTrash : MonoBehaviour
     public GameObject MainCamera;
     public GameObject Screen;
     public GameObject Monitor;
+    public GameObject[] thingsThatZoomOut;
     public float mindistance = 1F;
     public float ZoomSpeed = 0.02F;
     void OnTriggerEnter(Collider C)
     {
-        if (C.GetComponent<MakeNoiseOnSmack>() != null)
+        if (C.GetComponentInChildren<CanPickUp>() != null)
         {
-            C.transform.parent = transform;
-            if (transform.childCount > 3 && Bug.activeSelf && Monitor.GetComponent<Rigidbody>().isKinematic)
+            C.transform.SetParent(transform);
+        }
+    }
+
+    void Update()
+    {
+        Debug.Log("children: "+transform.childCount);
+        Debug.Log("bug active: "+Bug.activeSelf);
+        Debug.Log("screen active: "+Screen.activeSelf);
+        Debug.Log("rb k:" + Monitor.GetComponent<Rigidbody>().isKinematic);
+        if (transform.childCount > 3 && !Bug.activeSelf && Monitor.GetComponent<Rigidbody>().isKinematic && Screen.activeSelf)
+        {
+            Debug.Log("Victory!");
+            if ((MainCamera.transform.position - Screen.transform.position).magnitude > mindistance)
             {
-                if ((MainCamera.transform.position - Screen.transform.position).magnitude > mindistance)
-                {
-                    MainCamera.transform.Translate(Vector3.forward * Time.deltaTime * ZoomSpeed);
-                }
+                MainCamera.transform.Translate(Vector3.forward * Time.deltaTime * ZoomSpeed);
+            }
+            foreach (var z in thingsThatZoomOut)
+            {
+                z.SetActive(false);
             }
         }
     }
