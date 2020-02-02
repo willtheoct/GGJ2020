9d7ec9a30403dfece198e23fef7a362c36d5faa2
Quang <quwanger@gmail.com>
2020-02-02 13:20:07 -0500

    Maybe finished end game code

--- Assets/code/ItemsInTrash.cs
+++ Assets/code/ItemsInTrash.cs
@@ -7,16 +7,17 @@ public class ItemsInTrash : MonoBehaviour
     public GameObject Bug;
     public GameObject MainCamera;
     public GameObject Screen;
-    public float maxdistance = 1F;
+    public GameObject Monitor;
+    public float mindistance = 1F;
     public float ZoomSpeed = 0.02F;
     void OnTriggerEnter(Collider C)
     {
         if (C.GetComponent<MakeNoiseOnSmack>() != null)
         {
             C.transform.parent = transform;
-            if (transform.childCount > 3 && Bug == null && )
+            if (transform.childCount > 3 && Bug.activeSelf && Monitor.GetComponent<Rigidbody>().isKinematic)
             {
-                if ((MainCamera.transform.position - Screen.transform.position).magnitude < maxdistance)
+                if ((MainCamera.transform.position - Screen.transform.position).magnitude > mindistance)
                 {
                     MainCamera.transform.Translate(Vector3.forward * Time.deltaTime * ZoomSpeed);
                 }
